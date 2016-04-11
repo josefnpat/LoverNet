1862726ee09c9a7579974d7e5f2ce6e3a2faf639
Seppi <seppi@josefnpat.com>
2016-04-11 15:19:36 -0500

    Changing all instances of conway's y,x to x,y to match the demo's style.
    While Lua may be written in C, which uses [Row-major order](https://en.wikipedia.org/wiki/Row-major_order), Lua uses [Iliffe vectors](https://en.wikipedia.org/wiki/Iliffe_vector), which are entirely Pointer based.
    
    Because of this reason, I choose to use x,y since it is easier to read from a mathematical perspective and it also matches the rest of the demo's style.
    
    If someone can still identify Row-major order in Lua's Iliffe vectors, I will happily revert these changes.

--- conway.lua
+++ conway.lua
@@ -5,11 +5,6 @@
 -- this functionality of the demo.
 --------------------------------------------------------------------------------
 
--- Indexes in the board are being accesed first height, second width, like [y][x]
--- if seen in screen coordinates or the more common (for programmers) [i][j], this
--- is because the cache misses are reduced by accessing adjacent locations more
--- often. (Ask your PC memory techie friends :^)
-
 local WHITE_COLOR = {r=255,g=255,b=255}
 
 -- This is a queue that will hold the changes needed to make the actual board look
@@ -20,12 +15,12 @@ for i = 1, 64*64 do
   changesQueue[i] = {}
 end
 
-function changesQueue:toggle(y, x, r, g, b, drawIndex)
+function changesQueue:toggle(x, y, r, g, b, drawIndex)
   drawIndex = drawIndex or 0
 
   self[0] = self[0] and self[0]+1 or 1
-  self[ self[0] ].y = y  -- This is not a typo
-  self[ self[0] ].x = x  -- This is not a typo
+  self[ self[0] ].y = y
+  self[ self[0] ].x = x
   self[ self[0] ].r = r
   self[ self[0] ].g = g
   self[ self[0] ].b = b
@@ -39,9 +34,9 @@ end
 -- This are some transient variables that will speed up things (maybe)
 local tmp,tmp2,tmp3
 
-local checkAlive = function( board, y, x, liveColor, drawIndex )
-  if type(board[y]) == 'table' and type(board[y][x]) == 'table' then
-    tmp3 = board[y][x]
+local checkAlive = function( board, x, y, liveColor, drawIndex )
+  if type(board[x]) == 'table' and type(board[x][y]) == 'table' then
+    tmp3 = board[x][y]
   else
     return false
   end
@@ -53,19 +48,19 @@ local checkAlive = function( board, y, x, liveColor, drawIndex )
   return false
 end
 
-local aliveNeighbours = function( board, y, x, liveColor )
+local aliveNeighbours = function( board, x, y, liveColor )
 
   -- Accumulator for the alive neighbours of a cell
   tmp2 = 0
 
-  if checkAlive( board, y-1, x-1, liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y-1, x  , liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y-1, x+1, liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y  , x-1, liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y  , x+1, liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y+1, x-1, liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y+1, x  , liveColor ) then tmp2 = tmp2 + 1 end
-  if checkAlive( board, y+1, x+1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x-1, y-1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x-1, y  , liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x-1, y+1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x  , y-1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x  , y+1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x+1, y-1, liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x+1, y  , liveColor ) then tmp2 = tmp2 + 1 end
+  if checkAlive( board, x+1, y+1, liveColor ) then tmp2 = tmp2 + 1 end
 
   return tmp2
 end
@@ -79,17 +74,17 @@ local gameOfLife = function( board, width, height, liveColor, drawIndex )
 
   changesQueue:clear()
 
-  for y = 1, height do
-    for x = 1, width do
-      tmp = aliveNeighbours( board, y, x, liveColor )
+  for x = 1, height do
+    for y = 1, width do
+      tmp = aliveNeighbours( board, x, y, liveColor )
 
-      if checkAlive(board, y, x, liveColor) then
+      if checkAlive(board, x, y, liveColor) then
         if tmp < 2 or tmp > 3 then
-          changesQueue:toggle( y, x, 0, 0, 0, drawIndex )
+          changesQueue:toggle( x, y, 0, 0, 0, drawIndex )
         end
       else
         if tmp == 3 then
-          changesQueue:toggle( y, x, liveColor.r, liveColor.g, liveColor.b, drawIndex )
+          changesQueue:toggle( x, y, liveColor.r, liveColor.g, liveColor.b, drawIndex )
         end
       end
 
@@ -99,17 +94,17 @@ local gameOfLife = function( board, width, height, liveColor, drawIndex )
   tmp = changesQueue[0]
 
   for i = 1, changesQueue[0] do
-    if not board[changesQueue[i].y] then
-      board[changesQueue[i].y] = {}
+    if not board[changesQueue[i].x] then
+      board[changesQueue[i].x] = {}
     end
-    if not board[changesQueue[i].y][changesQueue[i].x] then
-      board[changesQueue[i].y][changesQueue[i].x] = {}
+    if not board[changesQueue[i].x][changesQueue[i].y] then
+      board[changesQueue[i].x][changesQueue[i].y] = {}
     end
 
-    board[changesQueue[i].y][changesQueue[i].x].r = changesQueue[i].r
-    board[changesQueue[i].y][changesQueue[i].x].g = changesQueue[i].g
-    board[changesQueue[i].y][changesQueue[i].x].b = changesQueue[i].b
-    board[changesQueue[i].y][changesQueue[i].x].u = changesQueue[i].u
+    board[changesQueue[i].x][changesQueue[i].y].r = changesQueue[i].r
+    board[changesQueue[i].x][changesQueue[i].y].g = changesQueue[i].g
+    board[changesQueue[i].x][changesQueue[i].y].b = changesQueue[i].b
+    board[changesQueue[i].x][changesQueue[i].y].u = changesQueue[i].u
   end
   changesQueue:clear()
 
